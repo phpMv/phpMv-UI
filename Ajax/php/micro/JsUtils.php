4051fc854d5fe4f1c24ffab20d7d81bcb00c6dda
jcheron <myaddressmail@gmail.com>
2016-07-02 02:22:04 +0200

    micro JsUtils class (not tested)

---
+++ Ajax/php/micro/JsUtils.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Ajax\php\micro;
+
+use micro\controllers\Startup;
+use micro\utils\RequestUtils;
+class JsUtils extends \Ajax\JsUtils{
+
+	public function getUrl($url){
+		return RequestUtils::getUrl($url);
+	}
+
+	public function addViewElement($identifier,$content,&$view){
+		$controls=$view->getVar("q");
+		if (isset($controls) === false) {
+			$controls=array ();
+		}
+		$controls[$identifier]=$content;
+		$view->setVar("q", $controls);
+	}
+
+	public function createScriptVariable(&$view,$view_var, $output){
+		$view->setVar($view_var,$output);
+	}
+
+	public function forward($initialController,$controller,$action,$params=array()){
+		return $initialController->forward($controller,$action,$params,true,true,true);
+	}
+
+	public function renderContent($initialControllerInstance,$viewName, $params=NULL) {
+		return $initialControllerInstance->loadView($viewName,$params,true);
+	}
+
+	public function fromDispatcher($dispatcher){
+		return Startup::$urlParts;
+	}
+}
