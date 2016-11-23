350984880f932626fbc4f58647b819a35857e66b
jcheron <myaddressmail@gmail.com>
2016-11-23 23:05:18 +0100

    return types in doc Semantic file

--- Ajax/Semantic.php
+++ Ajax/Semantic.php
@@ -35,7 +35,7 @@ class Semantic extends BaseGui {
 	 *
 	 * @param string $identifier
 	 * @param array $items
-	 * @return Ajax\semantic\html\collections\HtmlMenu
+	 * @return Ajax\semantic\html\collections\menus\HtmlMenu
 	 */
 	public function htmlMenu($identifier, $items=array()) {
 		return $this->addHtmlComponent(new HtmlMenu($identifier, $items));
@@ -45,6 +45,7 @@ class Semantic extends BaseGui {
 	 * Adds an icon menu
 	 * @param string $identifier
 	 * @param array $items icons
+	 * @return Ajax\semantic\html\collections\menus\HtmlIconMenu
 	 */
 	public function htmlIconMenu($identifier, $items=array()) {
 		return $this->addHtmlComponent(new HtmlIconMenu($identifier, $items));
@@ -54,6 +55,7 @@ class Semantic extends BaseGui {
 	 * Adds an labeled icon menu
 	 * @param string $identifier
 	 * @param array $items icons
+	 * @return HtmlLabeledIconMenu
 	 */
 	public function htmlLabeledIconMenu($identifier, $items=array()) {
 		return $this->addHtmlComponent(new HtmlLabeledIconMenu($identifier, $items));
@@ -64,6 +66,7 @@ class Semantic extends BaseGui {
 	 * @param string $identifier
 	 * @param string $value
 	 * @param array $items
+	 * @return HtmlDropdown
 	 */
 	public function htmlDropdown($identifier, $value="", $items=array()) {
 		return $this->addHtmlComponent(new HtmlDropdown($identifier, $value, $items));
@@ -73,6 +76,7 @@ class Semantic extends BaseGui {
 	 * Adds a new message
 	 * @param string $identifier
 	 * @param string $content
+	 * @return HtmlMessage
 	 */
 	public function htmlMessage($identifier, $content="") {
 		return $this->addHtmlComponent(new HtmlMessage($identifier, $content));
@@ -82,6 +86,7 @@ class Semantic extends BaseGui {
 	 *
 	 * @param string $identifier
 	 * @param mixed $content
+	 * @return HtmlPopup
 	 */
 	public function htmlPopup(BaseHtml $container, $identifier, $content) {
 		return $this->addHtmlComponent(new HtmlPopup($container, $identifier, $content));
@@ -94,6 +99,7 @@ class Semantic extends BaseGui {
 	 * @param int $numCols
 	 * @param boolean $createCols
 	 * @param boolean $implicitRows
+	 * @return HtmlGrid
 	 */
 	public function htmlGrid($identifier, $numRows=1, $numCols=NULL, $createCols=true, $implicitRows=false) {
 		return $this->addHtmlComponent(new HtmlGrid($identifier, $numRows, $numCols, $createCols, $implicitRows));
@@ -133,11 +139,17 @@ class Semantic extends BaseGui {
 	 * Returns a new Semantic Form
 	 * @param string $identifier
 	 * @param array $elements
+	 * @return HtmlForm
 	 */
 	public function htmlForm($identifier, $elements=array()) {
 		return $this->addHtmlComponent(new HtmlForm($identifier, $elements));
 	}
 
+	/**
+	 * @param string $identifier
+	 * @param array $content
+	 * @return HtmlSticky
+	 */
 	public function htmlSticky($identifier, $content=array()) {
 		return $this->addHtmlComponent(new HtmlSticky($identifier, $content));
 	}
