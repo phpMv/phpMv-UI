5f7aeedd5a7b54bdf163c32e0b9323191f933695
jcheron <myaddressmail@gmail.com>
2017-03-13 12:37:29 +0100

    AjaxTrait $rowclass

--- Ajax/common/traits/JqueryAjaxTrait.php
+++ Ajax/common/traits/JqueryAjaxTrait.php
@@ -162,9 +162,10 @@ trait JqueryAjaxTrait {
 	 * @param string $method Method use
 	 * @param string $jsCallback javascript code to execute after the request
 	 * @param string $context jquery DOM element, array container.
+	 * @param string $rowClass the css class for the new element
 	 * @param boolean $immediatly
 	 */
-	public function _jsonArray($maskSelector, $url, $method="get", $params="{}", $jsCallback=NULL, $attr="id", $context=null,$immediatly=false) {
+	public function _jsonArray($maskSelector, $url, $method="get", $params="{}", $jsCallback=NULL, $attr="id", $context=null,$rowClass="_json",$immediatly=false) {
 		$jsCallback=isset($jsCallback) ? $jsCallback : "";
 		$retour=$this->_getAjaxUrl($url, $attr);
 		if($context===null){
@@ -178,7 +179,7 @@ trait JqueryAjaxTrait {
 		$retour.="var self = $(this);\n$.{$method}(url,".$params.").done(function( data ) {\n";
 		$retour.=$parent.".find('._json').remove();";
 		$retour.="\tdata=$.parseJSON(data);$.each(data, function(index, value) {\n"."\tvar created=false;var maskElm=$('".$maskSelector."').first();maskElm.hide();"."\tvar newId=(maskElm.attr('id') || 'mask')+'-'+index;"."\tvar newElm=".$newElm.";\n"."\tif(!newElm.length){\n"."\t\tnewElm=maskElm.clone();
-				newElm.attr('id',newId);\n;newElm.addClass('_json').removeClass('_jsonArrayModel');\nnewElm.find('[id]').each(function(){ var newId=$(this).attr('id')+'-'+index;$(this).attr('id',newId).removeClass('_jsonArrayChecked');});\n";
+				newElm.attr('id',newId);\n;newElm.addClass('{$rowClass}').removeClass('_jsonArrayModel');\nnewElm.find('[id]').each(function(){ var newId=$(this).attr('id')+'-'+index;$(this).attr('id',newId).removeClass('_jsonArrayChecked');});\n";
 		$retour.= $appendTo;
 		$retour.="\t}\n"."\tfor(var key in value){\n"."\t\t\tvar html = $('<div />').append($(newElm).clone()).html();\n"."\t\t\tif(html.indexOf('__'+key+'__')>-1){\n"."\t\t\t\tcontent=$(html.split('__'+key+'__').join(value[key]));\n"."\t\t\t\t$(newElm).replaceWith(content);newElm=content;\n"."\t\t\t}\n"."\t\tvar sel='[data-id=\"'+key+'\"]';if($(sel,newElm).length){\n"."\t\t\tvar selElm=$(sel,newElm);\n"."\t\t\t if(selElm.is('[value]')) { selElm.attr('value',value[key]);selElm.val(value[key]);} else { selElm.html(value[key]); }\n"."\t\t}\n"."}\n"."\t$(newElm).show(true);"."\n"."\t$(newElm).removeClass('hide');"."});\n";
 		$retour.="\t$(document).trigger('jsonReady',[data]);\n";
