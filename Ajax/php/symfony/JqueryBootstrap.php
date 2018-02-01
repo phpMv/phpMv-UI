421c615171fc6bf0657d68b385649651a6022c81
jcheron <myaddressmail@gmail.com>
2018-02-01 13:33:15 +0100

    new scrut runner

--- Ajax/php/symfony/JqueryBootstrap.php
+++ Ajax/php/symfony/JqueryBootstrap.php
@@ -4,7 +4,7 @@ namespace Ajax\php\symfony;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class JquerySemantic extends Jquery_ {
+class JqueryBootstrap extends Jquery_ {
 
 	public function __construct(RouterInterface $router,ContainerInterface $container ){
 		parent::__construct(["bootstrap"=>true,"defer"=>true],$router);
