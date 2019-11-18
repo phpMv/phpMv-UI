170c0da24f777bb0789e619abef9d8f66a9f9ff6
jcheron <myaddressmail@gmail.com>
2019-11-18 07:55:27 +0100

    Update Toast.php

--- Ajax/semantic/components/Toast.php
+++ Ajax/semantic/components/Toast.php
@@ -25,37 +25,52 @@ class Toast extends SimpleSemExtComponent {
 
 	public function setClass($value){
 		$this->params['class']=$value;
+		return $this;
 	}
 	
 	public function setCloseIcon(){
 		$this->params['closeIcon']=true;
+		return $this;
 	}
 	
 	public function setShowIcon($value=false){
 		$this->params['showIcon']=$value;
+		return $this;
 	}
 	
 	public function setCloseOnClick($value){
 		$this->params['closeOnClick']=$value;
+		return $this;
 	}
+	
 	public function setTitle($title){
 		$this->params['title']=$title;
+		return $this;
 	}
 	
 	public function setMessage($message){
 		$this->params['message']=$message;
+		return $this;
+	}
+	
+	public function setPosition($position){
+		$this->params['position']=$position;
+		return $this;
 	}
 	
 	public function setDisplayTime($time){
 		$this->params['displayTime']=$time;
+		return $this;
 	}
 	
 	public function setShowProgress($value='top'){
 		$this->params['showProgress']=$value;
+		return $this;
 	}
 	
 	public function setClassProgress($value){
 		$this->params['classProgress']=$value;
+		return $this;
 	}
 
 	public function setOnShow($jsCode) {
@@ -64,13 +79,16 @@ class Toast extends SimpleSemExtComponent {
 	
 	public function setOnHide($jsCode) {
 		$this->addComponentEvent('onHide', $jsCode);
+		return $this;
 	}
 	
 	public function setOnApprove($jsCode) {
 		$this->addComponentEvent('onApprove', $jsCode);
+		return $this;
 	}
 	
 	public function setOnDeny($jsCode) {
 		$this->addComponentEvent('onDeny', $jsCode);
+		return $this;
 	}
 }
