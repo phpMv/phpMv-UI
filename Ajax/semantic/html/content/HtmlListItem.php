fdcf9573c0b78100631e1877fbcec782169f146a
jcheron <myaddressmail@gmail.com>
2019-11-28 03:12:49 +0100

    Update HtmlListItem.php

--- Ajax/semantic/html/content/HtmlListItem.php
+++ Ajax/semantic/html/content/HtmlListItem.php
@@ -1,62 +1,64 @@
 <?php
-
 namespace Ajax\semantic\html\content;
-
 
 use Ajax\service\JArray;
 use Ajax\semantic\html\elements\HtmlList;
+
 class HtmlListItem extends HtmlAbsractItem {
+
 	protected $image;
 
-	public function __construct($identifier, $content=NULL) {
-		parent::__construct($identifier,"item",$content);
+	public function __construct($identifier, $content = NULL) {
+		parent::__construct($identifier, "item", $content);
 	}
-	protected function initContent($content){
-		if(\is_array($content)){
-			if(JArray::isAssociative($content)===false){
-				$icon=@$content[0];
-				$title=@$content[1];
-				$desc=@$content[2];
-			}else{
-				$icon=@$content["icon"];
-				$image=@$content["image"];
-				$title=@$content["title"];
-				$header=@$content["header"];
-				$desc=@$content["description"];
-				$items=@$content["items"];
+
+	protected function initContent($content) {
+		if (\is_array($content)) {
+			if (JArray::isAssociative($content) === false) {
+				$icon = $content[0] ?? null;
+				$title = $content[1] ?? null;
+				$desc = $content[2] ?? null;
+			} else {
+				$icon = $content["icon"] ?? null;
+				$image = $content["image"] ?? null;
+				$title = $content["title"] ?? null;
+				$header = $content["header"] ?? null;
+				$desc = $content["description"] ?? null;
+				$items = $content["items"] ?? null;
 			}
-			if(isset($icon)){
+			if (isset($icon)) {
 				$this->setIcon($icon);
 			}
-			if(isset($image)){
+			if (isset($image)) {
 				$this->setImage($image);
 			}
-			if(isset($title)){
-				$this->setTitle($title,$desc);
-			}elseif (isset($header)){
-				$this->setTitle($header,$desc,"header");
+			if (isset($title)) {
+				$this->setTitle($title, $desc);
+			} elseif (isset($header)) {
+				$this->setTitle($header, $desc, "header");
 			}
-			if(isset($items)){
+			if (isset($items)) {
 				$this->addList($items);
 			}
-		}else{
+		} else {
 			$this->setContent($content);
 		}
 	}
-	public function addList($items=array(),$ordered=false) {
-		$list=new HtmlList("", $items);
-		if($ordered)
+
+	public function addList($items = array(), $ordered = false) {
+		$list = new HtmlList("", $items);
+		if ($ordered)
 			$list->setOrdered();
 		$list->setClass("list");
-		$this->content["list"]=$list;
+		$this->content["list"] = $list;
 		return $list;
 	}
 
-	public function getList(){
+	public function getList() {
 		return $this->content["list"];
 	}
 
-	public function getItem($index){
+	public function getItem($index) {
 		return $this->getList()->getItem($index);
 	}
 }
