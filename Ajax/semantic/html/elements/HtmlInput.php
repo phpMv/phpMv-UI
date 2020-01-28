e7f8f8585c6d796887b9af52848b43421ec349ac
jcheron <myaddressmail@gmail.com>
2020-01-28 02:57:59 +0100

    Add Datalist to HtmlInput

--- Ajax/semantic/html/elements/HtmlInput.php
+++ Ajax/semantic/html/elements/HtmlInput.php
@@ -10,17 +10,26 @@ use Ajax\semantic\html\collections\form\traits\FieldTrait;
 use Ajax\JsUtils;
 use Ajax\common\html\html5\HtmlInput as HtmlInput5;
 use Ajax\service\Javascript;
+use Ajax\semantic\html\elements\html5\HtmlDatalist;
 
 class HtmlInput extends HtmlSemDoubleElement {
 	use IconTrait,TextFieldsTrait,FieldTrait;
 
-	public function __construct($identifier, $type="text", $value="", $placeholder="") {
+	public function __construct($identifier, $type = "text", $value = "", $placeholder = "") {
 		parent::__construct("div-" . $identifier, "div", "ui input");
-		$this->_identifier=$identifier;
-		$this->_libraryId=$identifier;
-		$this->content=[ "field" => new HtmlInput5($identifier, $type, $value, $placeholder) ];
-		$this->_states=[ State::DISABLED,State::FOCUS,State::ERROR ];
-		$this->_variations=[ Variation::TRANSPARENT ];
+		$this->_identifier = $identifier;
+		$this->_libraryId = $identifier;
+		$this->content = [
+			"field" => new HtmlInput5($identifier, $type, $value, $placeholder)
+		];
+		$this->_states = [
+			State::DISABLED,
+			State::FOCUS,
+			State::ERROR
+		];
+		$this->_variations = [
+			Variation::TRANSPARENT
+		];
 	}
 
 	public function getField() {
@@ -31,27 +40,35 @@ class HtmlInput extends HtmlSemDoubleElement {
 		return $this->content["field"];
 	}
 
-	public static function outline($identifier, $icon, $value="", $placeholder="") {
-		$result=new HtmlInput($identifier, "text", $value, $placeholder);
+	public static function outline($identifier, $icon, $value = "", $placeholder = "") {
+		$result = new HtmlInput($identifier, "text", $value, $placeholder);
 		$result->addToProperty("class", "transparent");
 		$result->addIcon($icon)->setOutline();
 		return $result;
 	}
 
 	public function run(JsUtils $js) {
-		$result=parent::run($js);
-		$result->attach("#" . $this->getDataField()->getIdentifier());
+		$result = parent::run($js);
+		$result->attach("#" . $this->getDataField()
+			->getIdentifier());
 		return $result;
 	}
 
-	public function setTransparent(){
+	public function setTransparent() {
 		return $this->addToProperty("class", "transparent");
 	}
-	
-	public function compile_once(\Ajax\JsUtils $js = NULL, &$view = NULL){
-		parent::compile_once($js,$view);
-		if(isset($this->content['file'])){
+
+	public function compile_once(\Ajax\JsUtils $js = NULL, &$view = NULL) {
+		parent::compile_once($js, $view);
+		if (isset($this->content['file'])) {
 			$this->onCreate(Javascript::fileUploadBehavior($this->identifier));
 		}
+	}
+
+	public function addDataList($items) {
+		$dl = new HtmlDatalist('list-' . $this->identifier);
+		$dl->addItems($items);
+		$this->getDataField()->setProperty('list', $dl->getIdentifier());
+		$this->getDataField()->wrap($dl);
 	}
 }
