6da0828af7096936e63a6915e63a688e75b8fef7
jcheron <myaddressmail@gmail.com>
2020-04-27 03:21:28 +0200

    Fix checkedlist items count pb

--- Ajax/semantic/components/jsTemplates/tplCheckedList.php
+++ Ajax/semantic/components/jsTemplates/tplCheckedList.php
@@ -1,8 +1,17 @@
 <?php
-return '$("%identifier% .master")
+return '
+var deferredEach=function (arr, batchSize) {
+var deferred = $.Deferred();var index = 0;
+function chunk () {var lastIndex = Math.min(index + batchSize, arr.length);
+	for(;index<lastIndex;index++){deferred.notify(index, arr[index]);}
+    if (index >= arr.length) {deferred.resolve();} else {setTimeout(chunk, 0);}
+};
+setTimeout(chunk, 0);return deferred.promise();
+};
+$("%identifier% .master")
   .checkbox({
-    onChecked: function() {if(!$(this).hasClass("_notAllChecked")){$(this).closest(".checkbox").siblings(".list").find(".checkbox").checkbox("check");}},
-    onUnchecked: function() {$(this).closest(".checkbox").siblings(".list").find(".checkbox").checkbox("uncheck");}
+    onChecked: function() {if($(this).closest(".loading").length==0 && !$(this).hasClass("_notAllChecked")){$(this).closest(".checkbox").siblings(".list").find(".checkbox").checkbox("check");}},
+    onUnchecked: function() {if($(this).closest(".loading").length==0){$(this).closest(".checkbox").siblings(".list").find(".checkbox").checkbox("uncheck");}}
   })
 ;
 $("%identifier% .list .child.checkbox")
@@ -14,14 +23,17 @@ $("%identifier% .list .child.checkbox")
         $checkbox       = $listGroup.find(".checkbox"),
         allChecked      = true,
         allUnchecked    = true;
-      $checkbox.each(function() {
-        if( $(this).checkbox("is checked") ) {
+		$listGroup.closest(".segment").addClass("loading");
+		deferredEach($checkbox,5).progress(function(index, item){
+        if( $(item).checkbox("is checked") ) {
           allUnchecked = false;
         }
         else {
           allChecked = false;
         }
-      });
+		if(!allUnchecked && !allChecked) return;
+      }).done(function(){
+		$listGroup.closest(".segment.loading").removeClass("loading");
       if(allChecked) {
         $parentCheckbox.checkbox("set checked");
       }
@@ -34,6 +46,7 @@ $("%identifier% .list .child.checkbox")
         $parentCheckbox.checkbox("set indeterminate");
       }
 		%onChange%
+	});
     }
   })
 ;';
--- Ajax/semantic/html/elements/HtmlList.php
+++ Ajax/semantic/html/elements/HtmlList.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Ajax\semantic\html\elements;
 
 use Ajax\semantic\html\base\HtmlSemCollection;
@@ -8,34 +7,41 @@ use Ajax\semantic\html\collections\form\HtmlFormCheckbox;
 use Ajax\JsUtils;
 use Ajax\semantic\html\modules\checkbox\AbstractCheckbox;
 use Ajax\semantic\components\Visibility;
+use Ubiquity\utils\base\UString;
 
 class HtmlList extends HtmlSemCollection {
+
 	protected $_hasCheckedList;
-	protected $_fireOnInit=true;
-	protected $_ckItemChange="";
+
+	protected $_fireOnInit = false;
+
+	protected $_ckItemChange = "";
+
 	protected $_maxVisible;
+
 	protected $_dataList;
+
 	protected $_visibility;
 
-	public function __construct($identifier, $items=array()) {
+	public function __construct($identifier, $items = array()) {
 		parent::__construct($identifier, "div", "ui list");
 		$this->addItems($items);
-		$this->_hasCheckedList=false;
+		$this->_hasCheckedList = false;
 	}
 
 	protected function createItem($value) {
-		$count=$this->count();
-		$item=new HtmlListItem("item-" . $this->identifier . "-" . $count, $value);
+		$count = $this->count();
+		$item = new HtmlListItem("item-" . $this->identifier . "-" . $count, $value);
 		return $item;
 	}
 
 	public function addHeader($niveau, $content) {
-		$header=new HtmlHeader("header-" . $this->identifier, $niveau, $content, "page");
+		$header = new HtmlHeader("header-" . $this->identifier, $niveau, $content, "page");
 		$this->wrap($header);
 		return $header;
 	}
 
-	public function getItemPart($index,$partName="header"){
+	public function getItemPart($index, $partName = "header") {
 		return $this->getItem($index)->getPart($partName);
 	}
 
@@ -43,27 +49,30 @@ class HtmlList extends HtmlSemCollection {
 		return $this->contentAs($tagName);
 	}
 
-	public function asLinks($hrefs=[],$target=NUll) {
-		$this->addToPropertyCtrl("class", "link", array ("link" ));
-		return parent::asLinks($hrefs,$target);
+	public function asLinks($hrefs = [], $target = NUll) {
+		$this->addToPropertyCtrl("class", "link", array(
+			"link"
+		));
+		return parent::asLinks($hrefs, $target);
 	}
 
-	public function addList($items=array()) {
-		$list=new HtmlList("", $items);
+	public function addList($items = array()) {
+		$list = new HtmlList("", $items);
 		$list->setClass("list");
 		return $this->addItem($list);
 	}
-	
+
 	/**
+	 *
 	 * @return HtmlListItem
 	 */
-	public function getItem($index){
+	public function getItem($index) {
 		return parent::getItem($index);
 	}
 
-	protected function getItemToAdd($item){
-		$itemO=parent::getItemToAdd($item);
-		if($itemO instanceof AbstractCheckbox)
+	protected function getItemToAdd($item) {
+		$itemO = parent::getItemToAdd($item);
+		if ($itemO instanceof AbstractCheckbox)
 			$itemO->addClass("item");
 		return $itemO;
 	}
@@ -82,33 +91,38 @@ class HtmlList extends HtmlSemCollection {
 
 	public function run(JsUtils $js) {
 		if ($this->_hasCheckedList === true) {
-			$jsCode=include dirname(__FILE__) . '/../../components/jsTemplates/tplCheckedList.php';
-			$jsCode=\str_replace("%identifier%", "#" . $this->identifier, $jsCode);
-			$jsCode=\str_replace("%fireOnInit%", $this->_fireOnInit, $jsCode);
-			$jsCode=\str_replace("%onChange%", $this->_ckItemChange, $jsCode);
+			$jsCode = include dirname(__FILE__) . '/../../components/jsTemplates/tplCheckedList.php';
+			$jsCode = \str_replace("%identifier%", "#" . $this->identifier, $jsCode);
+			$jsCode = \str_replace("%fireOnInit%", $this->_fireOnInit, $jsCode);
+			$jsCode = \str_replace("%onChange%", $this->_ckItemChange, $jsCode);
 			$this->executeOnRun($jsCode);
 		}
-		/*if(isset($this->_visibility)){
-			$this->_visibility->run($js);
-		}*/
+		/*
+		 * if(isset($this->_visibility)){
+		 * $this->_visibility->run($js);
+		 * }
+		 */
 		return parent::run($js);
 	}
-	
-	protected function addFollowPoints(){
-		$count=$this->count();
-		for ($i=$this->_maxVisible;$i<$count;$i++){
-			$this->getItem($i)->addClass("notVisible")->setProperty("style", "display: none;");
+
+	protected function addFollowPoints() {
+		$count = $this->count();
+		for ($i = $this->_maxVisible; $i < $count; $i ++) {
+			$this->getItem($i)
+				->addClass("notVisible")
+				->setProperty("style", "display: none;");
 		}
-		$item=$this->addItem("...");
+		$item = $this->addItem("...");
 		$item->addClass("points");
-		$item->onClick('$(this).hide();$("#'.$this->identifier.' .notVisible").show();');
+		$item->onClick('$(this).hide();$("#' . $this->identifier . ' .notVisible").show();');
 	}
-	
-	public function onCkItemChange($jsCode){
-		$this->_ckItemChange=$jsCode;
+
+	public function onCkItemChange($jsCode) {
+		$this->_ckItemChange = $jsCode;
 	}
 
 	/**
+	 *
 	 * @param boolean $fireOnInit
 	 */
 	public function setFireOnInit($fireOnInit) {
@@ -132,88 +146,93 @@ class HtmlList extends HtmlSemCollection {
 	}
 
 	/**
-	 * {@inheritDoc}
+	 *
+	 * {@inheritdoc}
 	 * @see \Ajax\common\html\HtmlCollection::compile()
 	 */
 	public function compile(JsUtils $js = NULL, &$view = NULL) {
-		if(isset($this->_maxVisible) && $this->_maxVisible<$this->count()){
+		if (isset($this->_maxVisible) && $this->_maxVisible < $this->count()) {
 			$this->addFollowPoints();
-			if(isset($js)){
-				$visibility=new Visibility($js);
-				$visibility->attach("#".$this->identifier);
+			if (isset($js)) {
+				$visibility = new Visibility($js);
+				$visibility->attach("#" . $this->identifier);
 				$visibility->setOnTopVisible("$(this).children('.notVisible').hide();$(this).find('.points').show();");
-				$visibility->compile($js,$view);
-				$this->_visibility=$visibility;
+				$visibility->compile($js, $view);
+				$this->_visibility = $visibility;
 			}
 		}
-		return parent::compile ($js,$view);
+		return parent::compile($js, $view);
 	}
 
 	/**
 	 * Adds a grouped checked box to the list
+	 *
 	 * @param array $items
 	 * @param string|array|null $masterItem
 	 * @param array $values
 	 * @param string $notAllChecked
 	 * @return HtmlList
 	 */
-	public function addCheckedList($items=array(), $masterItem=NULL, $values=array(),$notAllChecked=false,$name=null) {
-		$count=$this->count();
-		$identifier=$this->identifier . "-" . $count;
+	public function addCheckedList($items = array(), $masterItem = NULL, $values = array(), $notAllChecked = false, $name = null) {
+		$count = $this->count();
+		$identifier = $this->identifier . "-" . $count;
 		if (isset($masterItem)) {
-			if(\is_array($masterItem)){
-				$masterO=new HtmlFormCheckbox("master-" . $identifier, @$masterItem[0],@$masterItem[1]);
-				if(isset($name))
+			if (\is_array($masterItem)) {
+				$masterO = new HtmlFormCheckbox("master-" . $identifier, @$masterItem[0], @$masterItem[1]);
+				if (isset($name))
 					$masterO->setName($name);
-				if(isset($masterItem[1])){
-					if(\array_search($masterItem[1], $values)!==false){
+				if (isset($masterItem[1])) {
+					if (\array_search($masterItem[1], $values) !== false) {
 						$masterO->getDataField()->setProperty("checked", "");
 					}
 				}
-			}else{
-				$masterO=new HtmlFormCheckbox("master-" . $identifier, $masterItem);
+			} else {
+				$masterO = new HtmlFormCheckbox("master-" . $identifier, $masterItem);
 			}
-			if($notAllChecked){
+			if ($notAllChecked) {
 				$masterO->getDataField()->addClass("_notAllChecked");
 			}
 			$masterO->getHtmlCk()->addToProperty("class", "master");
 			$masterO->setClass("item");
 			$this->addItem($masterO);
 		}
-		$fields=array ();
-		$i=0;
-		foreach ( $items as $val => $caption ) {
-			$itemO=new HtmlFormCheckbox($identifier . "-" . $i++, $caption, $val, "child");
+		$fields = array();
+		$i = 0;
+		$this->setFireOnInit(UString::getBooleanStr($this->_fireOnInit || count($items) < 30));
+		foreach ($items as $val => $caption) {
+			$itemO = new HtmlFormCheckbox($identifier . "-" . $i ++, $caption, $val, "child");
 			if (\array_search($val, $values) !== false) {
 				$itemO->getDataField()->setProperty("checked", "");
 			}
-			if(isset($name))
+			if (isset($name))
 				$itemO->setName($name);
 			$itemO->setClass("item");
-			$fields[]=$itemO;
+			$fields[] = $itemO;
 		}
 		if (isset($masterO) === true) {
-			$list=new HtmlList("", $fields);
+			$list = new HtmlList("", $fields);
 			$list->setClass("list");
 			$masterO->addContent($list);
 		} else {
 			$this->addList($fields);
 		}
-		$this->_hasCheckedList=true;
+		$this->_hasCheckedList = true;
 		return $this;
 	}
 
-	public function setIcons($icons){
-		if(!\is_array($icons)){
-			$icons=\array_fill(0, \sizeof($this->content), $icons);
+	public function setIcons($icons) {
+		if (! \is_array($icons)) {
+			$icons = \array_fill(0, \sizeof($this->content), $icons);
 		}
-		$max=\min(\sizeof($icons),\sizeof($this->content));
-		for($i=0;$i<$max;$i++){
+		$max = \min(\sizeof($icons), \sizeof($this->content));
+		for ($i = 0; $i < $max; $i ++) {
 			$this->content[$i]->addIcon($icons[$i]);
 		}
 		return $this;
 	}
+
 	/**
+	 *
 	 * @return mixed
 	 */
 	public function getMaxVisible() {
@@ -221,11 +240,11 @@ class HtmlList extends HtmlSemCollection {
 	}
 
 	/**
+	 *
 	 * @param mixed $_maxVisible
 	 */
 	public function setMaxVisible($_maxVisible) {
 		$this->_maxVisible = $_maxVisible;
 		return $this;
 	}
-
 }
